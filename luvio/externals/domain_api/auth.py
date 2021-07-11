8e75bea4171fc2367cc6d7ebd5a19382932840d5
Long Nguyen <nguyenhailong253@gmail.com>
2021-07-11 19:49:58 +1000

    retrieve access token from domain

--- luvio/externals/domain_api/auth.py
+++ luvio/externals/domain_api/auth.py
@@ -1,1 +1,26 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+import base64
+
 domain_auth_url = 'https://auth.domain.com.au/v1/connect/token'
+
+class DomainApiService:
+    def __init__(self):
+        self.access_token = os.getenv("DOMAIN_ACCESS_TOKEN")
+        if not self.access_token:
+            self.access_token = self.refresh_token()
+        # Check env variable for access token. If non, call refresh token method. Otherwise just use it
+        # When calling Domain API, if receive 401, meaning token might have expired, try refresh token and then re call
+
+    def refresh_token(self):
+        client_id = os.getenv('DOMAIN_CLIENT_ID')
+        client_secrets = os.getenv('DOMAIN_CLIENT_SECRETS')
+
+        req_body = {
+            'scope': 'api_agencies_read api_listings_read api_properties_read api_addresslocators_read api_demographics_read api_locations_read api_salesresults_read api_suburbperformance_read',
+            'grant_type': 'client_credentials'
+        }
+
+        response = requests.post(domain_auth_url, data=req_body, auth=HTTPBasicAuth(client_id, client_secrets))
+        return response.json()['access_token']
--- luvio/modules/properties/views.py
+++ luvio/modules/properties/views.py
@@ -1,15 +1,17 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from externals.domain_api.auth import DomainApiService
 
 class PropertyList(APIView):
     """
     [Return list of properties]
     """
+
     def post(self, request, format=None):
+        domain_api = DomainApiService()
         # Parse request body to get a list of searched suburbs
-        suburbs = request.data["suburbs"]
-        print(suburbs)
+        # suburbs = request.data["suburbs"]
         # Send GET request to Domain website to get a list of result listings
         # Convert listings to list of property ids
         # Call Domain API using those ids
